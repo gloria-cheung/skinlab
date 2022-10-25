61167f04bca874e75a206432e7ee5537d690671c
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-24 20:03:14 -0400

    create home page and import topbar and announcement component

---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import Topbar from "../components/Topbar";
+import Announcement from "../components/Announcement";
+const Home = () => {
+  return (
+    <div>
+      <Announcement />
+      <Topbar />
+      homepage
+    </div>
+  );
+};
+
+export default Home;
