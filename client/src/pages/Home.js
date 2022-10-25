3bbb88ef2486f097aa5c057e0fb401c3226a0710
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-24 21:26:54 -0400

    import categories component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,14 @@
 import Topbar from "../components/Topbar";
 import Announcement from "../components/Announcement";
 import Slider from "../components/Slider";
+import Categories from "../components/Categories";
 const Home = () => {
   return (
     <>
       <Announcement />
       <Topbar />
       <Slider />
+      <Categories />
     </>
   );
 };
