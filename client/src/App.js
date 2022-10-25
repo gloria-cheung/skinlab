6a2f11028833d0fb5fe026c7592cc2034c44fd2d
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-25 16:25:46 -0400

    import Login and Register page components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <Product />
+      <Register />
     </div>
   );
 }
