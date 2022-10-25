acdb55f82d60274b474ca2bd8420036b39192ef9
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-24 20:04:45 -0400

    create announcement component to display alerts and go above topbar

---
+++ client/src/components/Announcement.js
@@ -0,0 +1,10 @@
+import { Alert } from "react-bootstrap";
+const Announcement = () => {
+  return (
+    <Alert variant="dark" className="pt-1 pb-1 mb-0 text-center">
+      Free Shipping on All Orders Over $100
+    </Alert>
+  );
+};
+
+export default Announcement;
