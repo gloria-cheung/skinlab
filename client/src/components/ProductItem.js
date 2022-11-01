b1fb0e8e4fb09941f0c805d81d53e38d3fc83742
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-11-01 16:14:46 -0400

    when click shoppingcart icon, dispatch to update cart using redux with qty = 1

--- client/src/components/ProductItem.js
+++ client/src/components/ProductItem.js
@@ -6,17 +6,26 @@ import {
 } from "@material-ui/icons";
 import "./ProductItem.scss";
 import { Link } from "react-router-dom";
+import { addProduct } from "../redux/cartRedux";
+import { useDispatch } from "react-redux";
 
 const ProductItem = (props) => {
-  const { img, _id } = props;
+  const { img, _id, price } = props;
+  const dispatch = useDispatch();
 
+  const handleClick = () => {
+    dispatch(addProduct({ product: props, quantity: 1, price }));
+  };
   return (
     <Col lg={3} className="p-1">
       <Card className="productItemContainer">
         <img src={img} alt="productitem" className="productItem" />
         <div className="productItemIcons">
           <FavoriteBorderOutlined className="productItemIcon" />
-          <ShoppingCartOutlined className="productItemIcon" />
+          <ShoppingCartOutlined
+            className="productItemIcon"
+            onClick={handleClick}
+          />
           <Link to={`/product/${_id}`}>
             <Search className="productItemIcon" />
           </Link>
