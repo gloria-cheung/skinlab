5a06e7f73175681de622fdf8d9f470453a60ad74
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-11-02 11:18:20 -0400

    use redux to get total of cart

--- client/src/components/OrderSummary.js
+++ client/src/components/OrderSummary.js
@@ -1,36 +1,42 @@
 import { Card, Button } from "react-bootstrap";
+import { useSelector } from "react-redux";
 import "./OrderSummary.scss";
 
 const OrderSummary = () => {
+  const total = useSelector((state) => state.cart.total);
+
   return (
-    <Card className="border-0">
-      <Card.Body>
-        <Card.Title className="orderSummaryTitle mb-4">
-          Order Summary
-        </Card.Title>
-        <Card.Text className="orderSummaryLine">
-          <span>Subtotal:</span>
-          <span>$80.99</span>
-        </Card.Text>
-        <Card.Text className="orderSummaryLine">
-          <span>Estimated Shipping:</span>
-          <span>$6.99</span>
-        </Card.Text>
-        <Card.Text className="orderSummaryLine">
-          <span>Shipping Discount:</span>
-          <span>$-6.99</span>
-        </Card.Text>
-        <Card.Text className="orderSummaryLine orderSummaryTotal">
-          <span>Total:</span>
-          <span>$80.99</span>
-        </Card.Text>
-        <div className="d-grid gap-2">
-          <Button variant="dark" className="checkoutButton">
-            Checkout Now
-          </Button>
-        </div>
-      </Card.Body>
-    </Card>
+    <>{total ? (
+      <Card className="border-0">
+        <Card.Body>
+          <Card.Title className="orderSummaryTitle mb-4">
+            Order Summary
+          </Card.Title>
+          <Card.Text className="orderSummaryLine">
+            <span>Subtotal:</span>
+            <span>${total}</span>
+          </Card.Text>
+          <Card.Text className="orderSummaryLine">
+            <span>Estimated Shipping:</span>
+            <span>$6.99</span>
+          </Card.Text>
+          <Card.Text className="orderSummaryLine">
+            <span>Shipping Discount:</span>
+            <span>$-6.99</span>
+          </Card.Text>
+          <Card.Text className="orderSummaryLine orderSummaryTotal">
+            <span>Total:</span>
+            <span>${total}</span>
+          </Card.Text>
+          <div className="d-grid gap-2">
+            <Button variant="dark" className="checkoutButton">
+              Checkout Now
+            </Button>
+          </div>
+        </Card.Body>
+      </Card>
+    ): <div></div>}</>
+   
   );
 };
 
