2ef3831a7b233890a369991b69f48145f036faf7
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-11-02 12:05:20 -0400

    dispatch reducer when click + and - button per line item in cart

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -1,8 +1,22 @@
 import { Container, Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import {
+  updateProductAddOne,
+  updateProductSubtractOne,
+} from "../redux/cartRedux";
 import "./CartItem.scss";
 
 const CartItem = (props) => {
   const { _id, title, price, size, img, quantity } = props;
+  const dispatch = useDispatch();
+
+  const handleAddQuantity = () => {
+    dispatch(updateProductAddOne({ ...props, quantity: quantity + 1 }));
+  };
+
+  const handleMinusQuantity = () => {
+    dispatch(updateProductSubtractOne({ ...props, quantity: quantity - 1 }));
+  };
 
   return (
     <Container className="cartBody">
@@ -21,18 +35,26 @@ const CartItem = (props) => {
         </p>
         <div className="bottomCartMobile">
           <div className="addRemove">
-            <Button variant="light">-</Button>
-            <span>1</span>
-            <Button variant="light">+</Button>
+            <Button variant="light" onClick={handleMinusQuantity}>
+              -
+            </Button>
+            <span>{quantity}</span>
+            <Button variant="light" onClick={handleAddQuantity}>
+              +
+            </Button>
           </div>
           <div className="cartItemPrice">${price}</div>
         </div>
       </div>
       <div className="rightCart">
         <div className="addRemove">
-          <Button variant="light">-</Button>
+          <Button variant="light" onClick={handleMinusQuantity}>
+            -
+          </Button>
           <span>{quantity}</span>
-          <Button variant="light">+</Button>
+          <Button variant="light" onClick={handleAddQuantity}>
+            +
+          </Button>
         </div>
         <div className="cartItemPrice">${price}</div>
       </div>
