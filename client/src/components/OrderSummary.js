33514fcfe8a485e5d33f66163d2e6b268a55d71a
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-11-02 12:05:35 -0400

    round total to 2 decimal places

--- client/src/components/OrderSummary.js
+++ client/src/components/OrderSummary.js
@@ -6,37 +6,40 @@ const OrderSummary = () => {
   const total = useSelector((state) => state.cart.total);
 
   return (
-    <>{total ? (
-      <Card className="border-0">
-        <Card.Body>
-          <Card.Title className="orderSummaryTitle mb-4">
-            Order Summary
-          </Card.Title>
-          <Card.Text className="orderSummaryLine">
-            <span>Subtotal:</span>
-            <span>${total}</span>
-          </Card.Text>
-          <Card.Text className="orderSummaryLine">
-            <span>Estimated Shipping:</span>
-            <span>$6.99</span>
-          </Card.Text>
-          <Card.Text className="orderSummaryLine">
-            <span>Shipping Discount:</span>
-            <span>$-6.99</span>
-          </Card.Text>
-          <Card.Text className="orderSummaryLine orderSummaryTotal">
-            <span>Total:</span>
-            <span>${total}</span>
-          </Card.Text>
-          <div className="d-grid gap-2">
-            <Button variant="dark" className="checkoutButton">
-              Checkout Now
-            </Button>
-          </div>
-        </Card.Body>
-      </Card>
-    ): <div></div>}</>
-   
+    <>
+      {total ? (
+        <Card className="border-0">
+          <Card.Body>
+            <Card.Title className="orderSummaryTitle mb-4">
+              Order Summary
+            </Card.Title>
+            <Card.Text className="orderSummaryLine">
+              <span>Subtotal:</span>
+              <span>${Math.round(total * 100) / 100}</span>
+            </Card.Text>
+            <Card.Text className="orderSummaryLine">
+              <span>Estimated Shipping:</span>
+              <span>$6.99</span>
+            </Card.Text>
+            <Card.Text className="orderSummaryLine">
+              <span>Shipping Discount:</span>
+              <span>$-6.99</span>
+            </Card.Text>
+            <Card.Text className="orderSummaryLine orderSummaryTotal">
+              <span>Total:</span>
+              <span>${Math.round(total * 100) / 100}</span>
+            </Card.Text>
+            <div className="d-grid gap-2">
+              <Button variant="dark" className="checkoutButton">
+                Checkout Now
+              </Button>
+            </div>
+          </Card.Body>
+        </Card>
+      ) : (
+        <div></div>
+      )}
+    </>
   );
 };
 
