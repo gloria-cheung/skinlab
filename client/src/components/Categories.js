b5fb859afd4c76f31db156972fbf8daee1508cd4
gloria <gloriacheung812@gmail.com>
2022-11-21 18:05:06 -0500

    make axios call to api/categories to fetch categories data and render each category item

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -1,33 +1,39 @@
 import { Container, Row } from "react-bootstrap";
 import CategoryItem from "./CategoryItem";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Categories = () => {
-  const fakeData = [
-    {
-      id: 1,
-      img: "https://images.unsplash.com/photo-1564020426549-fabfb8c467ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzZ8fHNraW5jYXJlJTIwcHJvZHVjdHN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
-      title: "Dullness",
-    },
-    {
-      id: 2,
-      img: "https://images.unsplash.com/photo-1619451334792-150fd785ee74?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8c2tpbmNhcmUlMjBwcm9kdWN0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
-      title: "Acne",
-    },
-    {
-      id: 3,
+  const [categories, setCategories] = useState([]);
 
-      img: "https://images.unsplash.com/photo-1620916297397-a4a5402a3c6c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2tpbmNhcmUlMjBwcm9kdWN0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
-      title: "Dryness",
-    },
-  ];
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get("/categories");
+        setCategories(res.data.categories);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-  const categories = fakeData.map((item) => (
-    <CategoryItem key={item.id} {...item} />
-  ));
+    fetchData();
+  }, []);
+
+  const category_imgs = {
+    1: "https://images.unsplash.com/photo-1564020426549-fabfb8c467ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzZ8fHNraW5jYXJlJTIwcHJvZHVjdHN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+    2: "https://images.unsplash.com/photo-1619451334792-150fd785ee74?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8c2tpbmNhcmUlMjBwcm9kdWN0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+    3: "https://images.unsplash.com/photo-1620916297397-a4a5402a3c6c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2tpbmNhcmUlMjBwcm9kdWN0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+  };
+
+  const categories_data =
+    categories.length &&
+    categories.map((item) => (
+      <CategoryItem key={item.id} {...item} img={category_imgs[item.id]} />
+    ));
 
   return (
     <Container className="mt-5 mb-5">
-      <Row>{categories}</Row>
+      <Row>{categories_data}</Row>
     </Container>
   );
 };
