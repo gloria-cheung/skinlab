66491d11b493f0ef7e2e487c763ae41c91b0dac9
gloria <gloriacheung812@gmail.com>
2022-11-21 18:10:02 -0500

    remove size and price options

--- client/src/components/ProductDetails.js
+++ client/src/components/ProductDetails.js
@@ -9,8 +9,6 @@ import { useDispatch } from "react-redux";
 const ProductDetails = () => {
   const [product, setProduct] = useState();
   const [quantity, setQuantity] = useState(1);
-  const [size, setSize] = useState();
-  const [price, setPrice] = useState();
   const { id } = useParams();
   const dispatch = useDispatch();
 
@@ -19,9 +17,6 @@ const ProductDetails = () => {
       try {
         const res = await axios.get(`/products/${id}`);
         setProduct(res.data);
-        // default to regular size and price
-        setPrice(res.data.price[1]);
-        setSize(res.data.size[1]);
       } catch (err) {
         console.log(err);
       }
@@ -40,43 +35,26 @@ const ProductDetails = () => {
   };
 
   const handleClick = () => {
-    dispatch(addProduct({ ...product, quantity, size, price }));
+    // dispatch(addProduct({ ...product, quantity, size, price }));
   };
-  const handleSize = (e) => {
-    if (e.target.value === "regular") {
-      setSize("regular");
-      setPrice(product.price[1]);
-    } else if (e.target.value === "travel") {
-      setSize("travel");
-      setPrice(product.price[0]);
-    } else if (e.target.value === "value") {
-      setSize("value");
-      setPrice(product.price[2]);
-    }
-  };
+
   console.log(product);
   return (
     <Container className="d-flex mt-5 productDetailsContainer">
       {product && (
         <>
           <div className="leftContainer">
-            <img src={product.img} alt="productImg" className="productImg" />
+            <img
+              src={product.img_url}
+              alt="productImg"
+              className="productImg"
+            />
           </div>
           <div className="rightContainer">
-            <h2>{product.title}</h2>
+            <h2>{product.name}</h2>
             <p>{product.desc}</p>
-            <h4>${price}</h4>
-            <select
-              name="size"
-              id="filter-size"
-              onChange={(e) => handleSize(e)}
-              defaultValue={"regular"}
-            >
-              <option value="">Size</option>
-              <option value="travel">Travel Size</option>
-              <option value="regular">Regular Size</option>
-              <option value="value">Value Size</option>
-            </select>
+            <h4>${product.price}</h4>
+
             <div className="cartButtons">
               <div className="addRemove">
                 <Button variant="light" onClick={handleSubtractQuantity}>
