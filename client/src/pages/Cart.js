b569bb26a228a8406c2cb2e5d978fac734872d17
gloria <gloriacheung812@gmail.com>
2022-11-22 17:02:18 -0500

    fetch data using axios for cart data and pass as props to cart container and order summary

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -5,8 +5,33 @@ import OrderSummary from "../components/OrderSummary";
 import CartHeader from "../components/CartHeader";
 import CartContainer from "../components/CartContainer";
 import { Row, Col } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Cart = () => {
+  const [cart, setCart] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get("/cart");
+        setCart(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const updateCart = async () => {
+    try {
+      const res = await axios.get("/cart");
+      setCart(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
       <Announcement />
@@ -14,11 +39,9 @@ const Cart = () => {
       <CartHeader />
       <Row className="w-100">
         <Col md={8}>
-          <CartContainer />
+          <CartContainer cart={cart} updateCart={updateCart} />
         </Col>
-        <Col md={4}>
-          <OrderSummary />
-        </Col>
+        <Col md={4}>{cart && <OrderSummary total={cart.cart.total} />}</Col>
       </Row>
       <Footer />
     </>
