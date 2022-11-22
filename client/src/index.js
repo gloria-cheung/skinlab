2f1fc00ad3ca870c597ca5ca62bf431f3413659c
gloria <gloriacheung812@gmail.com>
2022-11-22 17:00:02 -0500

    remove reducer

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { Provider } from "react-redux";
-import store from "./redux/store";
 import { AuthContextProvider } from "./context/AuthContext";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
-  </Provider>,
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>,
   document.getElementById("root")
 );
