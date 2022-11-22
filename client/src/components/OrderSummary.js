825cf2b06975cd0678a514989d0a49149536bd0b
gloria <gloriacheung812@gmail.com>
2022-11-22 17:02:37 -0500

    render total for cart using props

--- client/src/components/OrderSummary.js
+++ client/src/components/OrderSummary.js
@@ -1,44 +1,41 @@
+import { useEffect, useContext, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
 import { Card, Button } from "react-bootstrap";
-import { useSelector } from "react-redux";
 import "./OrderSummary.scss";
 
-const OrderSummary = () => {
-  const total = useSelector((state) => state.cart.total);
+const OrderSummary = (props) => {
+  const { total } = props;
 
   return (
     <>
-      {Math.round(total) ? (
-        <Card className="border-0">
-          <Card.Body>
-            <Card.Title className="orderSummaryTitle mb-4">
-              Order Summary
-            </Card.Title>
-            <Card.Text className="orderSummaryLine">
-              <span>Subtotal:</span>
-              <span>${Math.round(total * 100) / 100}</span>
-            </Card.Text>
-            <Card.Text className="orderSummaryLine">
-              <span>Estimated Shipping:</span>
-              <span>$6.99</span>
-            </Card.Text>
-            <Card.Text className="orderSummaryLine">
-              <span>Shipping Discount:</span>
-              <span>$-6.99</span>
-            </Card.Text>
-            <Card.Text className="orderSummaryLine orderSummaryTotal">
-              <span>Total:</span>
-              <span>${Math.round(total * 100) / 100}</span>
-            </Card.Text>
-            <div className="d-grid gap-2">
-              <Button variant="dark" className="checkoutButton">
-                Checkout Now
-              </Button>
-            </div>
-          </Card.Body>
-        </Card>
-      ) : (
-        <div></div>
-      )}
+      <Card className="border-0">
+        <Card.Body>
+          <Card.Title className="orderSummaryTitle mb-4">
+            Order Summary
+          </Card.Title>
+          <Card.Text className="orderSummaryLine">
+            <span>Subtotal:</span>
+            <span>${total}</span>
+          </Card.Text>
+          <Card.Text className="orderSummaryLine">
+            <span>Estimated Shipping:</span>
+            <span>$6.99</span>
+          </Card.Text>
+          <Card.Text className="orderSummaryLine">
+            <span>Shipping Discount:</span>
+            <span>$-6.99</span>
+          </Card.Text>
+          <Card.Text className="orderSummaryLine orderSummaryTotal">
+            <span>Total:</span>
+            <span>${total}</span>
+          </Card.Text>
+          <div className="d-grid gap-2">
+            <Button variant="dark" className="checkoutButton">
+              Checkout Now
+            </Button>
+          </div>
+        </Card.Body>
+      </Card>
     </>
   );
 };
