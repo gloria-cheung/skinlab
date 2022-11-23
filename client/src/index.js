5a1ae62f871d069f310e8468aaeee0ab0afb4db8
gloria <gloriacheung812@gmail.com>
2022-11-23 13:36:37 -0500

    import cart context and wrap app

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { AuthContextProvider } from "./context/AuthContext";
+import { AuthContextProvider } from "./context/auth/AuthContext";
+import { CartContextProvider } from "./context/cart/CartContext";
 
 ReactDOM.render(
   <AuthContextProvider>
-    <App />
+    <CartContextProvider>
+      <App />
+    </CartContextProvider>
   </AuthContextProvider>,
   document.getElementById("root")
 );
