ebce7da99d49e11bd497ab905626e74772f7f1cd
gloria <gloriacheung812@gmail.com>
2022-11-23 19:52:41 -0500

    make call to backend for stripe checkout and retrieve clientsecret to use in checkout form

--- client/src/components/OrderSummary.js
+++ client/src/components/OrderSummary.js
@@ -1,8 +1,39 @@
-import { Card, Button } from "react-bootstrap";
+import { useState } from "react";
+import { Card, Button, Modal } from "react-bootstrap";
+import axios from "axios";
+import CheckoutForm from "./CheckoutForm";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 import "./OrderSummary.scss";
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE);
 
 const OrderSummary = (props) => {
   const { total } = props;
+  const [clientSecret, setClientSecret] = useState("");
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const clickHandler = async () => {
+    handleShow();
+
+    try {
+      const res = await axios.post("/create-checkout-session");
+      setClientSecret(res.data.clientSecret);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  const appearance = {
+    theme: "stripe",
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
 
   return (
     <>
@@ -28,12 +59,28 @@ const OrderSummary = (props) => {
             <span>${total}</span>
           </Card.Text>
           <div className="d-grid gap-2">
-            <Button variant="dark" className="checkoutButton">
+            <Button
+              variant="dark"
+              className="checkoutButton"
+              onClick={clickHandler}
+            >
               Checkout Now
             </Button>
           </div>
         </Card.Body>
       </Card>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Checkout</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {clientSecret && (
+            <Elements options={options} stripe={stripePromise}>
+              <CheckoutForm />
+            </Elements>
+          )}
+        </Modal.Body>
+      </Modal>
     </>
   );
 };
