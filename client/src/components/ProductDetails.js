dd94d35b5f25669feba86bcebdaf543517d38dd8
gloria <gloriacheung812@gmail.com>
2022-11-24 19:49:11 -0500

    add button to add product to wishlist and display message if success or error after axios call

--- client/src/components/ProductDetails.js
+++ client/src/components/ProductDetails.js
@@ -1,14 +1,17 @@
-import { Container, Button } from "react-bootstrap";
+import { Container, Button, Alert } from "react-bootstrap";
 import { useParams, useHistory } from "react-router-dom";
 import { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { AuthContext } from "../context/auth/AuthContext";
 import { CartContext } from "../context/cart/CartContext";
+import { FavoriteBorderOutlined } from "@material-ui/icons";
 import "./ProductDetails.scss";
 
 const ProductDetails = () => {
   const [product, setProduct] = useState();
   const [quantity, setQuantity] = useState(1);
+  const [message, setMessage] = useState(null);
+  const [show, setShow] = useState(false);
   const { id } = useParams();
   const { currentUser } = useContext(AuthContext);
   const { cartDispatch } = useContext(CartContext);
@@ -20,7 +23,7 @@ const ProductDetails = () => {
         const res = await axios.get(`/products/${id}`);
         setProduct(res.data);
       } catch (err) {
-        console.log(err);
+        console.log(err.message);
       }
     };
     fetchData();
@@ -55,40 +58,83 @@ const ProductDetails = () => {
     }
   };
 
-  return (
-    <Container className="d-flex mt-5 productDetailsContainer">
-      {product && (
-        <>
-          <div className="leftContainer">
-            <img
-              src={product.img_url}
-              alt="productImg"
-              className="productImg"
-            />
-          </div>
-          <div className="rightContainer">
-            <h2>{product.name}</h2>
-            <p>{product.desc}</p>
-            <h4>${product.price}</h4>
+  const addToWishlist = async () => {
+    if (!currentUser) {
+      history.push("/login");
+    } else {
+      try {
+        const res = await axios.get("/wishlist");
+        const wishlist_id = res.data.wishlist.id;
 
-            <div className="cartButtons">
-              <div className="addRemove">
-                <Button variant="light" onClick={handleSubtractQuantity}>
-                  -
-                </Button>
-                <span>{quantity}</span>
-                <Button variant="light" onClick={handleAddQuantity}>
-                  +
+        await axios.post(`/wishlist/${wishlist_id}`, {
+          product_id: product.id,
+          wishlist_id: wishlist_id,
+        });
+        setMessage("Added to Wishlist");
+        setShow(true);
+      } catch (err) {
+        console.log(err.message);
+        setMessage("Something went wrong, please try again");
+        setShow(true);
+      }
+    }
+  };
+
+  return (
+    <>
+      {show && (
+        <Alert
+          className="text-center"
+          variant="dark"
+          onClose={() => setShow(false)}
+          dismissible
+        >
+          {message}
+        </Alert>
+      )}
+      <Container className="d-flex mt-5 productDetailsContainer">
+        {product && (
+          <>
+            <div className="leftContainer">
+              <img
+                src={product.img_url}
+                alt="productImg"
+                className="productImg"
+              />
+            </div>
+            <div className="rightContainer">
+              <h2>{product.name}</h2>
+              <p>{product.desc}</p>
+              <h4>${product.price}</h4>
+
+              <div className="cartButtons">
+                <div className="addRemove">
+                  <Button variant="light" onClick={handleSubtractQuantity}>
+                    -
+                  </Button>
+                  <span>{quantity}</span>
+                  <Button variant="light" onClick={handleAddQuantity}>
+                    +
+                  </Button>
+                </div>
+                <Button variant="secondary" onClick={handleClick}>
+                  Add to Cart
                 </Button>
               </div>
-              <Button variant="secondary" onClick={handleClick}>
-                Add to Cart
+
+              <Button
+                className="mt-3 ps-5 pe-5 wishlistButton"
+                variant="primary"
+                onClick={addToWishlist}
+              >
+                <FavoriteBorderOutlined className="wishlistIcon" />
+                Add to Wishlist
               </Button>
             </div>
-          </div>
-        </>
-      )}
-    </Container>
+          </>
+        )}
+      </Container>
+    </>
   );
 };
 
